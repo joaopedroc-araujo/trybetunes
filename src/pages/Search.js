908828286a9fc86f1542615bd8fa9be08e26b267
João Pedro <costaaraujojoaopedro@gmail.com>
2023-05-16 18:14:08 -0300

    FEAT: cria formulário para pesquisa de artistas ou bandas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
 class Search extends React.Component {
+  state = {
+    name: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
   render() {
+    const { name } = this.state;
+    const disabledBtn = name.length < 2;
     return (
-      <div data-testid="page-search" />
+      <div data-testid="page-search">
+        <form>
+          <label>
+            <input
+              onChange={ this.handleChange }
+              value={ name }
+              type="text"
+              data-testid="search-artist-input"
+              placeholder="Digite aqui o nome da banda ou artista"
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            disabled={ disabledBtn }
+          >
+            Pesquisar
+          </button>
+        </form>
+      </div>
     );
   }
 }

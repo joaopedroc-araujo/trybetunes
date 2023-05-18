9fd2ef0514e02bf843fba9c4e7dd04eecd304fd6
João Pedro <costaaraujojoaopedro@gmail.com>
2023-05-18 16:28:18 -0300

    FEAT: implementa botão de favoritar e desfavoritar

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Loading from '../pages/Loading';
 
 class MusicCard extends React.Component {
   state = {
     loading: false,
+    favorited: false,
   };
+
+  async componentDidMount() {
+    const favoritedSongs = await getFavoriteSongs();
+    const { music } = this.props;
+    const favoritedSong = favoritedSongs
+      .filter((favorite) => favorite.trackId === music.trackId).length > 0;
+    this.setState({ favorited: favoritedSong });
+  }
 
   handleCheckboxClick = async () => {
     const { music } = this.props;
+    const { favorited } = this.state;
     this.setState({ loading: true });
-    await addSong(music);
+    if (favorited) {
+      await removeSong(music);
+      this.setState({ favorited: false });
+    } else {
+      await addSong(music);
+      this.setState({ favorited: true });
+    }
+
     this.setState({ loading: false });
   };
 
   render() {
     const { music } = this.props;
     const { trackName, previewUrl, trackId } = music;
-    const { loading } = this.state;
+    const { loading, favorited } = this.state;
     return (
       <div>
         <h4>{trackName}</h4>
@@ -35,6 +52,7 @@ class MusicCard extends React.Component {
             data-testid={ `checkbox-music-${trackId}` }
             type="checkbox"
             disabled={ loading }
+            checked={ favorited }
           />
         </label>
         {loading && <Loading />}
